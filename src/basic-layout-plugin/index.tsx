5001195274cbedd043504677fb1e647ee7eb3938
Frank Aloisio <frank.aloisio@edduus.com>
2025-10-06 09:28:23 +1100

    added role to Editable for easier styling  - 1.1.0

--- src/basic-layout-plugin/index.tsx
+++ src/basic-layout-plugin/index.tsx
@@ -1,37 +1,38 @@
-import { clsx } from "clsx"
-import { useFocused } from "slate-react"
+import { clsx } from "clsx";
+import { useFocused } from "slate-react";
 
-import { createPlugin, RenderEditable } from "~/src/sink"
+import { createPlugin, RenderEditable } from "~/src/sink";
 
-import { $Editable } from "./styles"
+import { $Editable } from "./styles";
 
 export type BasicLayoutEditor = {
-  basicLayout: true
-}
+  basicLayout: true;
+};
 
 export type BasicLayoutPluginCustomTypes = {
-  Name: "basic-layout"
-  Editor: BasicLayoutEditor
-}
+  Name: "basic-layout";
+  Editor: BasicLayoutEditor;
+};
 
 export const BasicLayoutPlugin = () =>
   createPlugin<BasicLayoutPluginCustomTypes>((editor) => {
-    editor.basicLayout = true
+    editor.basicLayout = true;
     return {
       name: "basic-layout",
       renderEditable,
       editor: {},
       editableProps: {},
-    }
-  })
+    };
+  });
 
 const renderEditable: RenderEditable = ({ attributes, Editable }) => {
-  const focused = useFocused()
+  const focused = useFocused();
   return (
     <Editable
       className={clsx({ "--focused": focused })}
+      role="wysimark-editor"
       as={$Editable}
       {...attributes}
     />
-  )
-}
+  );
+};
